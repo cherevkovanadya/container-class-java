a82313cf33996f1c9df7ead91f8691a42a3557ca
Nadezhda <nadyachery@gmail.com>
2024-05-11 18:51:40 +0300

    Базовая реализация списка: добавление, удаление, извлечение одного элемента

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MyList list = new MyList();
+        list.add(10);
+        list.add(9);
+        list.add(8);
+        list.add(7);
+        list.add(6);
+        list.add(5);
+        list.add(1);
+        list.add(2);
+        list.print();
+        list.remove(0);
+        list.print();
+        System.out.println(list.get(5));
     }
 }
---
+++ src/main/java/org/example/MyList.java
@@ -0,0 +1,69 @@
+package org.example;
+
+public class MyList {
+    private Node head;
+    private int size;
+
+    public MyList() {
+        head = null;
+        size = 0;
+    }
+
+    private int size() {
+        return size;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        Node current = head;
+        for (int i = 0; i < index; ++i) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public void add(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        ++size;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        if (index == 0) {
+            int data = head.data;
+            head = head.next;
+            --size;
+            return data;
+        }
+        Node current = head;
+        for (int i = 0; i < index - 1; ++i) {
+            current = current.next;
+        }
+        int data = current.next.data;
+        current.next = current.next.next;
+        --size;
+        return data;
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
